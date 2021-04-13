7c4851ed0165f74d0507641ef2ef5c84eca655ad
Nhat Le <len2@mail.uc.edu>
2021-04-13 09:07:06 +0700

    Remove javascript shopping cart code

--- script/script.js
+++ script/script.js
@@ -1,8 +1,19 @@
+if ('serviceWorker' in navigator)
+{
+    window.addEventListener('load', () => {
+        navigator.serviceWorker
+            .register('../sw.js')
+            .then(reg => console.log('Service Worker: Registered'))
+            .catch(err => console.log(`Service Worker: Error: ${err}`))
+    })
+}
+
 function dropdownmenu() {
     var x = document.getElementById("navbar");
     if (x.className === "topnav") {
         x.className += " responsive";        
     } else {
         x.className = "topnav";
+
     }
 }
